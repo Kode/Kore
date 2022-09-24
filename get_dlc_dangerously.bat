e2846ff5c3be05bcd213243f8af892986bcf79e2
Robert Konrad <git2016@robdangero.us>
2022-09-24 20:42:18 +0200

    Keep Kinc on the main-branch when getting the dlc dangerously

--- get_dlc_dangerously.bat
+++ get_dlc_dangerously.bat
@@ -1,4 +1,9 @@
 @pushd "%~dp0"
+@if exist Tools\windows_x64\kmake.exe (
+@git submodule update --remote --merge Kinc
+) else (
 @git submodule update --init --remote Kinc
+@git -C Kinc checkout main
+)
 @call Kinc\get_dlc_dangerously.bat
 @popd
