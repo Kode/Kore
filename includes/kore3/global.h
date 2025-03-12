5dff45fca4c8f965af81a61d0d4ef4f96b889a24
Robert Konrad <git2016@robdangero.us>
2025-03-12 01:09:59 +0100

    Remove the old global.h namespace

--- includes/kore3/global.h
+++ includes/kore3/global.h
@@ -92,49 +92,6 @@
 #endif
 
 #ifdef __cplusplus
-
-namespace Kore {
-	typedef unsigned char u8;   // 1 Byte
-	typedef unsigned short u16; // 2 Byte
-	typedef unsigned int u32;   // 4 Byte
-
-#ifdef KORE_WINDOWS
-	typedef unsigned __int64 u64; // 8 Byte
-#else
-	typedef unsigned long long u64;
-#endif
-	typedef char s8;   // 1 Byte
-	typedef short s16; // 2 Byte
-	typedef int s32;   // 4 Byte
-#ifdef KORE_WINDOWS
-	typedef __int64 s64; // 8 Byte
-#else
-	typedef long long s64;
-#endif
-
-	typedef u32 uint; // 4 Byte
-	typedef s32 sint; // 4 Byte
-
-#ifdef KORE_64
-	typedef s64 spint;
-	typedef u64 upint;
-#else
-	typedef s32 spint;
-	typedef u32 upint;
-#endif
-}
-
-// pseudo C++11
-#if !defined(_MSC_VER) && __cplusplus <= 199711L
-#define nullptr 0
-#define override
-#endif
-
-#define Noexcept throw()
-
-#endif
-
-#ifdef __cplusplus
 extern "C" {
 #endif
 int kickstart(int argc, char **argv);
