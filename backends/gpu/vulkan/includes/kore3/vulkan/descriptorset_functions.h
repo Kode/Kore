5210f27bf388a6ebde244dff012c05a925fa2fba
Robbot <robbot2019@robdangero.us>
2025-05-26 05:17:39 +0000

    Format C

--- backends/gpu/vulkan/includes/kore3/vulkan/descriptorset_functions.h
+++ backends/gpu/vulkan/includes/kore3/vulkan/descriptorset_functions.h
@@ -21,8 +21,8 @@ void kore_vulkan_descriptor_set_set_storage_image_descriptor(kore_gpu_device *de
                                                              const kore_gpu_texture_view *texture_view, uint32_t index);
 void kore_vulkan_descriptor_set_set_sampled_image_array_descriptor(kore_gpu_device *device, kore_vulkan_descriptor_set *set,
                                                                    const kore_gpu_texture_view *texture_view, uint32_t index);
-void kore_vulkan_descriptor_set_set_sampled_cube_image_descriptor(kore_gpu_device *device, kore_vulkan_descriptor_set *set, const kore_gpu_texture_view *texture_view,
-                                                          uint32_t index);
+void kore_vulkan_descriptor_set_set_sampled_cube_image_descriptor(kore_gpu_device *device, kore_vulkan_descriptor_set *set,
+                                                                  const kore_gpu_texture_view *texture_view, uint32_t index);
 void kore_vulkan_descriptor_set_set_sampler(kore_gpu_device *device, kore_vulkan_descriptor_set *set, kore_gpu_sampler *sampler, uint32_t index);
 
 void kore_vulkan_descriptor_set_prepare_buffer(kore_gpu_command_list *list, kore_gpu_buffer *buffer, uint64_t offset, uint64_t size);
