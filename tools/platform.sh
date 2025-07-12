b135d8306cfb2b52b84bc778330cb9f39615f3f7
Robert Konrad <git2016@robdangero.us>
2025-07-12 19:19:36 +0200

    Fix the platform.sh comments

--- tools/platform.sh
+++ tools/platform.sh
@@ -7,7 +7,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	elif [[ "$MACHINE_TYPE" == "x86_64"* ]]; then
 		KORE_PLATFORM=linux_x64
 	else
-		echo "Unknown Linux machine '$MACHINE_TYPE', please edit Tools/platform.sh"
+		echo "Unknown Linux machine '$MACHINE_TYPE', please edit tools/platform.sh"
 		exit 1
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -17,7 +17,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	elif [[ "$MACHINE_TYPE" == "x86_64"* ]]; then
 		KORE_PLATFORM=macos_x64
 	else
-		echo "Unknown macOS machine '$MACHINE_TYPE', please edit Tools/platform.sh"
+		echo "Unknown macOS machine '$MACHINE_TYPE', please edit tools/platform.sh"
 		exit 1
 	fi
 elif [[ "$OSTYPE" == "FreeBSD"* ]]; then
@@ -26,6 +26,6 @@ elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* ]]; then
 	KORE_PLATFORM=windows_x64
 	KORE_EXE_SUFFIX=.exe
 else
-	echo "Unknown platform '$OSTYPE', please edit Tools/platform.sh"
+	echo "Unknown platform '$OSTYPE', please edit tools/platform.sh"
 	exit 1
 fi
