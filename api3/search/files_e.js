d593a986ecb56ada8a87cd9185e1cae656eb7eb2
Robbot <robbot2019@robdangero.us>
2025-09-11 00:02:20 +0000

    Update documentation to eb19f845981a78564e69338ec2b13bcdf478bbac.

--- api3/search/files_e.js
+++ api3/search/files_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['random_2eh_0',['random.h',['../random_8h.html',1,'']]],
-  ['rotation_2eh_1',['rotation.h',['../rotation_8h.html',1,'']]]
+  ['raytracing_2eh_1',['raytracing.h',['../raytracing_8h.html',1,'']]],
+  ['rotation_2eh_2',['rotation.h',['../rotation_8h.html',1,'']]]
 ];
