0e5f5b15aaa21f597cae2fc8bdccfb2027fba05b
Robert Konrad <git2016@robdangero.us>
2026-09-18 17:13:14 +0200

    Improve a comment

--- includes/kore3/io/filewriter.h
+++ includes/kore3/io/filewriter.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 
 /*! \file filewriter.h
-    \brief Provides an API very similar to fwrite and friends but uses a directory that can actually used for persistent file storage. This can later be read
+    \brief Provides an API very similar to fwrite and friends but uses a directory that provides persistent file storage. This can later be read
    using the kore_file_reader-functions and KORE_FILE_TYPE_SAVE.
 */
 
